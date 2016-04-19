0fbf132cbc9a5a8329cd0400c8e626be3d35d9c9
eli.boey <eli.boey@student.ehb.be>
2016-04-19 16:20:48 +0200

    Medicine Table View

--- HealthBuddy_iOS/PatientMedicineController.swift
+++ HealthBuddy_iOS/PatientMedicineController.swift
@@ -7,11 +7,44 @@
 //
 
 import UIKit;
-class PatientMedicineController: UIViewController {
-    var patient:User?;
+class PatientMedicineController: UITableViewController {
+    var patient:User!;
     
     override func viewDidLoad() {
         super.viewDidLoad();
         self.title = "Medicatie";
     }
+    
+    func backButtonPressed(sender:UIButton){
+        navigationController?.popViewControllerAnimated(true);
+    }
+    
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.patient.medicines!.count;
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("MedicineCell", forIndexPath: indexPath) as UITableViewCell;
+        let medicine:Medicine = self.patient.medicines![indexPath.row];
+        cell.textLabel?.text = medicine.name;
+        return cell;
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "loadMedicine" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                if let Controller = segue.destinationViewController as? PatientShowMedicineController {
+                    Controller.medicine =  self.patient.medicines![indexPath.row];
+                    Controller.patientID = self.patient.userId;
+                }
+            }
+        }
+        
+    }
+    
+    
 }
