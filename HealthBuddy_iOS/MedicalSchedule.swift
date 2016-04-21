0740db7e8a72deb0cba6c97c683663664243f598
J-Yen <Yen@MacBook-Pro-van-Yen.local>
2016-04-21 09:15:14 +0200

    NSDate datetransform bugfix

--- HealthBuddy_iOS/MedicalSchedule.swift
+++ HealthBuddy_iOS/MedicalSchedule.swift
@@ -44,6 +44,7 @@ class MedicalSchedule : Mappable {
         
         if(self.start_date_s != nil && self.end_date_s != nil){
             let dateFormatter = NSDateFormatter()
+            dateFormatter.timeZone = NSTimeZone(abbreviation: "GMT+0:00");
             dateFormatter.dateFormat = "yyyy-MM-dd"
             self.start_date = dateFormatter.dateFromString(self.start_date_s!);
             self.end_date = dateFormatter.dateFromString(self.end_date_s!);
@@ -51,12 +52,14 @@ class MedicalSchedule : Mappable {
         
         if(self.time_s != nil){
             let dateFormatter = NSDateFormatter()
+            dateFormatter.timeZone = NSTimeZone(abbreviation: "GMT+0:00");
             dateFormatter.dateFormat = "HH:mm:ss"
             self.time = dateFormatter.dateFromString(self.time_s!);
         }
         
         if(self.updated_at_s != nil){
             let dateFormatter = NSDateFormatter()
+            dateFormatter.timeZone = NSTimeZone(abbreviation: "GMT+0:00");
             dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss";
             self.updated_at = dateFormatter.dateFromString(self.updated_at_s!);
         }
